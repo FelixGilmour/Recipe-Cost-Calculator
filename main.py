3b1663c25ca94742820ab28ee0904020866b368b
FelixGilmour <127059201+FelixGilmour@users.noreply.github.com>
2024-03-13 21:29:29 +0000

    commit 2

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import time
 import random
 
+#start
+name = "Felix Gilmour"
+school = "James Hargest College"
+#constant
+course = "Programming Evidence Internal for 2.7 and 2.8 ~ 12 credits"
+due_date = "Due: 12 April 2024"
+print(f"Name: {name}\n"
+      f"{school}\n"
+      f"{course}\n"
+      f"{due_date}\n\n")
+time.sleep(1)
+
 #entername
+name_input = input("Enter your name: ")
+print(f"Welcome {name_input} to my Recipe Cost Calculator!\n")
 
 #special teacher powers
 
