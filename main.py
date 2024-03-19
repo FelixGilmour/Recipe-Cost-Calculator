6847c14806d0b2bf2ee8bba920c7cc1f073a0bab
FelixGilmour <127059201+FelixGilmour@users.noreply.github.com>
2024-03-19 22:30:17 +0000

    commit 4

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import sys
 #start
 name = "Felix Gilmour"
 school = "James Hargest College"
-#constant
 course = "Programming Evidence Internal for 2.7 and 2.8 ~ 12 credits"
 due_date = "Due: 12 April 2024"
 print(f"Name: {name}\n"
@@ -18,9 +17,23 @@ time.sleep(1)
 name_input = input("Enter your name: ")
 print(f"Welcome {name_input} to my Recipe Cost Calculator!\n")
 
-#special teacher powers
+#wait
+time.sleep(3)
+
+#recipename
+recipename_input = input("What is your recipe's name that you are calculating the cost of:  ")
+time.sleep(1)
+servingsize_input = input("How many servings are in your recipe:  ")
+print(f"Great! So you want: {recipename_input} and a serving size of {servingsize_input} for the {recipename_input}!\n")
+
+#wait
+time.sleep(3)
 
 #main code
+print("Okay, now we can get started!")
+time.sleep(3)
+ingredient_sizing = input("How many ingredients are in your recipe:  ")
+
 
 #inputs
 
