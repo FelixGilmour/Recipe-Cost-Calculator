2526957bfeb6fd8112f532f8e2d0f94260066b6f
FelixGilmour <127059201+FelixGilmour@users.noreply.github.com>
2024-04-05 01:51:08 +0000

    added and fixed code byt errrs til present

--- main.py
+++ main.py
@@ -8,21 +8,43 @@ course = "Programming Evidence Internal for 2.7 and 2.8 ~ 12 credits"
 due_date = "Due: 12 April 2024"
 
 #/n representing new lines
-print(f"Name: {name}\n"
+print(f"\033[4;31;40m Name: {name}\n"
       f"{school}\n"
       f"{course}\n"
       f"{due_date}\n\n")
 time.sleep(1)
 
+#age verfication
+
+while True:
+  print("\033[0;30;47m This is designed for people aged between 13 and 18 years old")
+  age_input = input("Please enter your age: ")
+  if not age_input.isdigit():
+    print("Please enter a valid age")
+  else:
+      age = int(age_input)
+      if age <13:
+        print("You are too young to use this program")
+      elif age >18:
+        print("You are too old to use this program")
+      else:
+        print("You are the correct age to use this program")
+        break
+
 #entername
-name_input = input("Enter your name: ")
+name_input = input("\033[4;31;40m Enter your name: ")
 print(f"Welcome {name_input} to my Recipe Cost Calculator!\n")
 
 #wait
 time.sleep(3)
 
 #recipename
-recipe_name = input("What is your recipe's name that you are calculating the cost of:  ")
+while True:
+  try:
+     recipe_name = input("What is your recipe's name that you are calculating the cost of:  ")
+  except ValueError:
+    print("Please enter a valid name")
+    break
 
 #wait
 time.sleep(3)
@@ -36,34 +58,71 @@ def create_recipe_table():
   ingredients = []
   #stores costs of individual ingredients
   ingredient_cost = []  
-  ingredient_count = int(input("\nHow many ingredients are in your recipe: "))
+  while True:
+    try:
+       ingredient_count = int(input("\033[1;34;40m \nHow many ingredients are in your recipe: "))
+    except ValueError:
+      print("Please enter a valid number")
+      break
+ 
 
   #collecting inputs
   for i in range(ingredient_count): 
-        
+     while True:      
       #Recipe Ingredients
       #collecting ingreient count and aadds +1 every time it loops
       print(f"\nEntering ingredient {i+1} details:")
       #printing recipe ingredients
       print("Recipe Ingredients")
+       
       #collecting the amount needed 1
-      amount_needed1 = float(input("Enter the recipe ingredient's, amount that you need number only: "))
+      try:
+         amount_needed1 = int(input("Enter the recipe ingredient's amount that you need, number only: "))
+      except ValueError:
+         print("Please enter a valid number")
+         continue
+        
       #collecting the unit 1
-      unit1 = input("Enter the recipe ingredient's unit (g/kg): ")
+      try: 
+         unit1 = input("Enter the recipe ingredient's unit (g/kg): ")
+      except ValueError:
+         print("Please enter a valid unit")
+         continue
+
       #collecting the name 1
-      name1 = input("Enter the name of the recipe ingredient: ")
+      try: 
+         name1 = input("Enter the name of the recipe ingredient: ")
+      except ValueError:
+         print("Please enter a valid unit")
+         continue
 
       #Ingredient Prices
       print("Ingredient Prices")
+       
       #collecting the price per unit 2
-      price_per_unit2 = float(input("Enter the price per unit of the ingredient price number only ($): "))
+      try: 
+         price_per_unit2 = int(input("Enter the price per unit of the ingredient price number only ($): "))
+      except ValueError:
+         print("Please enter a valid number")
+         continue
+       
       #collecting the amount needed 2
-      amount_needed2 = float(input("Enter the ingredient prices, amount that you need number only: "))
+      try:
+         amount_needed2 = int(input("Enter the ingredient prices, amount that you need number only: "))
+      except ValueError:
+         print("Please enter a valid number")
+         continue	
       #collecting the unit 2
-      unit2 = input("Enter the ingredient prices unit (g/kg): ")
+      try:
+         unit2 = input("Enter the ingredient prices unit (g/kg): ")
+      except ValueError:
+         print("Please enter a valid unit")
+         continue
+       
       #printting data base results
       print("We will now add this to the date base and caculate the cost to make, the total cost and cost per serve!")
-
+      
+  
       #converting the amounts as needed
       #converting to kg
       if unit1.lower() == 'g':
@@ -81,6 +140,7 @@ def create_recipe_table():
       cost = price_per_unit2 * amount_needed_kg1
       ingredient_cost.append(cost)  #cost of ingredients
       ingredients.append((amount_needed1, unit1, name1, price_per_unit2, amount_needed2, unit2))
+      break
 
   #servings
   serving = float(input("\nEnter the number of servings you want: "))
@@ -94,6 +154,7 @@ def create_recipe_table():
   
   #seps
   sep1 = '💵'
+  sep2 = '📝'
   
 
   #data
@@ -103,7 +164,7 @@ def create_recipe_table():
   print("Recipe Ingredients:")
   print(f"{'Amount':<10} {'Unit':<10} {'Ingredient':<20}")
   for amount1, unit1, name1, _, _, _ in ingredients:
-      print(f" {sep1} {amount1:<10} {unit1:<10} {name1:<20}")
+      print(f" {sep1} {amount1:<10} {unit1:<10} {sep2} {name1:<20}")
 
   #prints Ingredient Price
   print("Ingredient Price:")
